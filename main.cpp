7eefe3f81b5f882bad65bcda1683ff9d31212f9a
Joshua Nicholls <jnich@udel.edu>
2022-05-16 21:12:29 -0400

    added safety net to simulateInBetween for empty cpu

--- main.cpp
+++ main.cpp
@@ -217,6 +217,10 @@ void simulateInBetween(int currentTime, int duration){
     int simRunTime = 0;
     int nextCpuTime = 0;
     int jobFinishTime = 0;
+    if(currentSystem->getCurrentJob() == nullptr && readyQueue->head != nullptr){
+        QueueNode* first = readyQueue->deQueueTask();
+        currentSystem->updateCurrentJob(first);
+    }
     while(simRunTime < duration && currentSystem->getCurrentJob() != nullptr){
         nextCpuTime = simRunTime + currentSystem->getQuantumLeft();
         jobFinishTime = simRunTime + currentSystem->getCurrentJob()->job->getRemainingTime();
